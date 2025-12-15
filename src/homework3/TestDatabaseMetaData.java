d5c3248d7d45168c3dc1f75ba0b3d143560201ac
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-15 07:47:53 -0800

    add TestDatabaseMetaData

--- src/homework3/TestDatabaseMetaData.java
+++ src/homework3/TestDatabaseMetaData.java
@@ -1,6 +1,7 @@
 package homework3;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -14,5 +15,30 @@ public class TestDatabaseMetaData {
         Connection connection = DriverManager.getConnection
                 ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
         System.out.println("Database connected");
+
+        DatabaseMetaData dbMetaData = connection.getMetaData();
+        System.out.println("database URL: " + dbMetaData.getURL());
+        System.out.println("database username: " +
+                dbMetaData.getUserName());
+        System.out.println("database product name: " +
+                dbMetaData.getDatabaseProductName());
+        System.out.println("database product version: " +
+                dbMetaData.getDatabaseProductVersion());
+        System.out.println("JDBC driver name: " +
+                dbMetaData.getDriverName());
+        System.out.println("JDBC driver version: " +
+                dbMetaData.getDriverVersion());
+        System.out.println("JDBC driver major version: " +
+                dbMetaData.getDriverMajorVersion());
+        System.out.println("JDBC driver minor version: " +
+                dbMetaData.getDriverMinorVersion());
+        System.out.println("Max number of connections: " +
+                dbMetaData.getMaxConnections());
+        System.out.println("MaxTableNameLength: " +
+                dbMetaData.getMaxTableNameLength());
+        System.out.println("MaxColumnsInTable: " +
+                dbMetaData.getMaxColumnsInTable());
+
+        connection.close();
     }
 }
