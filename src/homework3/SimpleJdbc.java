6318066944490b1ae3d75f50b6524e3758911f35
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-15 07:20:01 -0800

    add SimpleJdbc

---
+++ src/homework3/SimpleJdbc.java
@@ -0,0 +1,18 @@
+package homework3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleJdbc {
+    public static void main(String[] args)
+        throws SQLException,ClassNotFoundException {
+
+        Class.forName("org.postgresql.Driver");
+        System.out.println("Driver loaded");
+
+        Connection connection = DriverManager.getConnection
+                ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
+        System.out.println("Database connected");
+    }
+}
