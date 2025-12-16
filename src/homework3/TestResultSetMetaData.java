b4dc1e31cc323dc5c95c7d66b53f2422c8dc6414
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 06:07:50 -0800

    add ResultSetMetaData

--- src/homework3/TestResultSetMetaData.java
+++ src/homework3/TestResultSetMetaData.java
@@ -10,12 +10,25 @@ public class TestResultSetMetaData {
         System.out.println("Driver loaded");
 
         Connection connection = DriverManager.getConnection
-                ("jdbc:postgresql://localhost:5432/javabook2","postges","postgres");
+                ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
         System.out.println("Database connected");
 
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery
                 ("select * from Student");
+
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        for (int i = 1; i < resultSetMetaData.getColumnCount(); i++)
+            System.out.printf("%-12s\t",resultSetMetaData.getColumnName(i));
+        System.out.println();
+
+        while (resultSet.next()) {
+            for (int i = 1; i < resultSetMetaData.getColumnCount(); i++)
+                System.out.printf("%-12s\t", resultSet.getObject(i));
+            System.out.println();
+        }
+
+        connection.close();
     }
 }
