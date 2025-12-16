1c15889429f6bd38b3c6c0a2ebe54f4d426dbd07
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 05:26:35 -0800

    add FindUserTables

--- src/homework3/FindUserTables.java
+++ src/homework3/FindUserTables.java
@@ -1,5 +1,7 @@
 package homework3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FindUserTables {
@@ -8,7 +10,11 @@ public class FindUserTables {
 
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded");
-        
-        
+
+        Connection connection = DriverManager.getConnection
+                ("jdbc:postgresql://localhost:5432/javabook2","postgres","postgres");
+        System.out.println("Database connected");
+
+
     }
 }
