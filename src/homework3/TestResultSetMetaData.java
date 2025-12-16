3742d56c1893c9c7a8cb44bc9f574733b6174e7e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2025-12-16 06:01:20 -0800

    add ResultSetMetaData

--- src/homework3/TestResultSetMetaData.java
+++ src/homework3/TestResultSetMetaData.java
@@ -1,5 +1,7 @@
 package homework3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TestResultSetMetaData {
@@ -8,5 +10,11 @@ public class TestResultSetMetaData {
 
         Class.forName("org.postgresql.Driver");
         System.out.println("Driver loaded");
+
+        Connection connection = DriverManager.getConnection
+                ("jdbc:postgresql://localhost:5432/javabook2","postges","postgres");
+        System.out.println("Database connected");
+
+
     }
 }
